667d9e4b0db8e11a745f7e2d46551cc0d043db03
Arnau Ramos <arnauramos21@gmail.com>
2024-03-23 13:36:44 +0100

    Added method to filter products by stock

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -5,23 +5,62 @@ const productsAPI = "https://deploying-json-api.adaptable.app/products";
 
 function Product() {
   const [products, setProducts] = useState([]);
+  const [productsFromAPI, setProductsFromAPI] = useState([]);
+  const [ inStockProducts, setInStockProducts ] = useState(false);
+  //const [ inStockProducts, setInStockProducts ] = useState([false]);
 
   useEffect(() => {
-    axios.get(productsAPI).then((response) => {
+    axios
+    .get(productsAPI)
+    .then((response) => {
       setProducts(response.data);
+      setProductsFromAPI(response.data);
     });
   }, []);
-  
+
+  useEffect(() => {
+    inStockFilter();
+  }, [inStockProducts]);
+
+  const inStockFilter = () => {
+/*     const inStock = productsFromAPI.filter((product) => {
+      return product.inStock
+    });
+    setProducts(inStock); */
+
+    //if the product is in stock, we filter the array and set Products to the filtered array.
+    if (inStockProducts) {
+      const inStock = productsFromAPI.filter((product) => {
+        return product.inStock;
+      });
+      setProducts(inStock);
+    } //if the product is not in stock, we filter the array and set Products to the origina 
+    //unfiltered array
+    else {
+      setProducts(productsFromAPI);
+    }
+  }
+
+  const handleCheckboxChange = () => {
+    setInStockProducts(!inStockProducts);
+  }
 
   return (
-      products.map((product) => (
-        <div className="product-container" key={product.id}>
-        {console.log(product.id)}
-          <h5>{product.description}</h5>
-          <p>{product.price}</p>
-        </div>
-      ))
-
+    <div className="products-list">
+      <ul>
+      {/* <input type="checkbox" onClick={inStockFilter}/> */}
+      {/* <span>Show Available<input type="checkbox"  onClick={handleCheckboxChange}/></span> */}
+      <label>Show Available</label>
+      <input type="checkbox"  onClick={handleCheckboxChange}/>
+        {products.map((product) => (
+          <li className="product-container" key={product.id}>
+            {console.log(product.id)}
+            <h5>{product.description}</h5>
+            <p>{product.price}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
